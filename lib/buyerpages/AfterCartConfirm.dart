4e6c29aaafe7b86c5830466bab7afccd8b7dfbcb
HarishanA21 <ambihaipahanharishan@gmail.com>
2024-07-16 15:52:42 +0530

    Hari - 1st commit

---
+++ lib/buyerpages/AfterCartConfirm.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:veg/buyerpages/Cart/coffee_tile.dart';
+import 'package:veg/buyerpages/Cart/model.dart';
+import 'package:veg/buyerpages/Cart/model_cart.dart';
+import 'package:veg/buyerpages/homepage_buyer.dart';
+
+class Aftercartconfirm extends StatefulWidget {
+  const Aftercartconfirm({Key? key}) : super(key: key);
+
+  @override
+  State<Aftercartconfirm> createState() => _AftercartconfirmState();
+}
+
+class _AftercartconfirmState extends State<Aftercartconfirm> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
